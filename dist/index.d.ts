832e3c2dabaaff2947d5885cf1c073fc24e9d122
Arie Trouw <arie.trouw@xylabs.com>
2022-01-26 18:00:58 -0800

    deps cleanup

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import '@rushstack/eslint-patch/modern-module-resolution';
-declare const _default: {
-    extends: string[];
-    parserOptions: {
-        tsconfigRootDir: string;
-    };
-};
-export = _default;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// eslint-disable-next-line import/no-internal-modules
-require("@rushstack/eslint-patch/modern-module-resolution");
-var process_1 = require("process");
-module.exports = {
-    extends: ['@xylabs', './react'],
-    parserOptions: { tsconfigRootDir: (0, process_1.cwd)() },
-};
-//# sourceMappingURL=index.js.map
