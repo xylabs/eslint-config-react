bb70e47e0d348c5ba6a3e9d6e48eb55b6cd48399
Arie Trouw <arie.trouw@xylabs.com>
2022-03-21 14:50:26 -0700

    yarn 3

--- src/react/index.ts
+++ src/react/index.ts
@@ -2,6 +2,12 @@ export = {
   extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
   plugins: ['react', 'react-hooks'],
   rules: {
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: '(useAsyncEffect)',
+      },
+    ],
     'react/prop-types': ['off'],
     'react/react-in-jsx-scope': ['off'],
   },
